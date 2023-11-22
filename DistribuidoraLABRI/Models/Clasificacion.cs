7fff20279170b63a7fa9d59df84c00702d30ace9
BrisaOrtega <ortegabrisa27@gmail.com>
2023-11-21 22:34:40 -0300

    arreglos

---
+++ DistribuidoraLABRI/Models/Clasificacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DistribuidoraLABRI.Models
+{
+    [Table("Clasificacion")]
+    public class Clasificacion
+    {
+        [Key]
+        [Column("ID")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor, ingresar la clasificacion")]
+        [Display(Name = "Clasificacion")]
+        [StringLength(50)]
+        public string? Clasificaciones { get; set; }
+
+        [Column(TypeName = "smalldatetime")]
+        [DataType(DataType.Date)]
+        public DateTime? FechaRegistro { get; set; }
+    }
+    
+}
---
+++ DistribuidoraLABRI/repos/DistribuidoraContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using DistribuidoraLABRI.Models;
+using DistribuidoraLABRI.repos.Models;
+using NPOI.SS.Formula.Functions;
+using static NPOI.HSSF.Util.HSSFColor;
+
+namespace DistribuidoraLABRI.repos
+{
+    public partial class DistribuidoraContext:DbContext
+    {
+        public DistribuidoraContext()
+        {
+        }
+
+        public DistribuidoraContext(DbContextOptions<DistribuidoraContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<CondicionPago> CondicionPagos { get; set; }
+        public virtual DbSet<ListaPrecio> ListaPrecios { get; set; }
+        public virtual DbSet<Clasificacion> Clasificaciones { get; set; }
+        public virtual DbSet<Marcas> Marca { get; set; }
+        public virtual DbSet<NombreProductos> NombreProducto { get; set; }
+        public virtual DbSet<VentasProducto> VentasProductos { get; set; }
+        public virtual DbSet<Productos> Producto { get; set; }
+        public virtual DbSet<Codigo> Codigos { get; set; }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //=> optionsBuilder.UseSqlServer("name=conexion");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+
+}
